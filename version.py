90af51c028751ef038e1c839e722d89e18f76a0d
tang-shuyuan <126368294+tang-shuyuan@users.noreply.github.com>
2025-03-05 01:16:40 +0800

    Update version.py

--- version.py
+++ version.py
@@ -3,7 +3,6 @@
 
 def get_versions():
     return versions[0]["number"]
-<<<<<<< HEAD
 versions=[{"number":"1.0.7","question":"add two genome compare"},
           {"number":"1.0.6","question":"support windows,update quality control"},
           {"number":"1.0.5","question":"add data statistics information"},
@@ -11,9 +10,4 @@ versions=[{"number":"1.0.7","question":"add two genome compare"},
           {"number":"1.0.3","question":"Redesign the software into 3 modules"},
           {"number":"1.0.2","question":"The description of the software"},
           {"number":"1.0.1","question":"change language"},
-=======
-versions=[{"number":"1.0.3","question":"Redesign the software into 3 modules"},\
-          {"number":"1.0.2","question":"The description of the software"},\
-          {"number":"1.0.1","question":"change language"},\
->>>>>>> 862aae7ed2d6d4c8522547cd44f43d5037b4ab35
           {"number":"1.0.0","question":"no question"}]
