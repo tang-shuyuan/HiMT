7f611304ed5fcdfa24e7ea606bee4a1290c9aacc
tang-shuyuan <126368294+tang-shuyuan@users.noreply.github.com>
2026-06-13 13:36:43 +0800

    Update HiMT code files

--- version.py
+++ version.py
@@ -3,13 +3,17 @@
 
 def get_versions():
     return versions[0]["number"]
-versions=[{"number":"1.0.9","question":"auto assess. assess and compare module adjust ,refassemble improvement"},
-          {"number":"1.0.8","question":"added refassemble and refactored the assess module"},
-          {"number":"1.0.7","question":"added two genome compare"},
-          {"number":"1.0.6","question":"support windows,update quality control"},
-          {"number":"1.0.5","question":"added data statistics information"},
-          {"number":"1.0.4","question":"assessment module adjust"},
+versions=[{"number":"1.1.3","question":"Localize bandage and add animal assesement"},
+          {"number":"1.1.2","question":"Rewrite filter"},
+          {"number":"1.1.1","question":"Fix refassemble"},
+          {"number":"1.1.0","question":"Support doenload graph in svg format"},
+          {"number":"1.0.9","question":"Auto assess. assess and compare module adjust ,refassemble improvement"},
+          {"number":"1.0.8","question":"Added refassemble and refactored the assess module"},
+          {"number":"1.0.7","question":"Added two genome compare"},
+          {"number":"1.0.6","question":"Support windows,update quality control"},
+          {"number":"1.0.5","question":"Added data statistics information"},
+          {"number":"1.0.4","question":"Assessment module adjust"},
           {"number":"1.0.3","question":"Redesign the software into 3 modules"},
           {"number":"1.0.2","question":"The description of the software"},
-          {"number":"1.0.1","question":"change language"},
-          {"number":"1.0.0","question":"no question"}]
+          {"number":"1.0.1","question":"Change language"},
+          {"number":"1.0.0","question":"No question"}]
